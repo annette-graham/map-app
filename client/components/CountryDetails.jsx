42f3ed556331d3fc860010650a2a64d547c9af4f
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-04-19 18:40:37 +1200

    trying to get details to render

--- client/components/CountryDetails.jsx
+++ client/components/CountryDetails.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import WorldMap from './WorldMap'
 
 export default function CountryDetails (props) {
-  const {country, isVisible, hideDetails} = props
+  const {data, isVisible, hideDetails} = props
   const classes = 'country-details ' + (isVisible ? 'visible' : 'hidden')
 
   return (
     <div className='classes'>
-      <h2>{props.name}</h2>
+        {console.log("THIS " + {data})}
+      <h2>hi</h2>
     </div>
   )
 
